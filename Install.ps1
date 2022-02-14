4bb67b6125cba12ca1d2cb9b3b0b05ab35f0fb82
NSimic <nsimic@gmail.com>
2022-02-15 00:19:56 +0100

    feat: Change port availability check - changed pre-flight port check - simplified the instructions to make install more understandable

--- Install.ps1
+++ Install.ps1
@@ -4,6 +4,7 @@ if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
     Exit;
 }
 
+$dockerFtwVersion = "0.0.1"
 $distro = "docker-ftw";
 $dockerPort = 2376;
 $alpineVersion = "3.15";
@@ -11,31 +12,56 @@ $alpineVersionFull = "3.15.0";
 $dockerCliVersion = "20.10.9";
 $arch = "x86_64";
 
+$ErrorActionPreference = "SilentlyContinue"
+ 
+function CheckPortIsAvailable {
+    param([string] $Address = "localhost", $Port = 2376, $Timeout = 3000)
+
+    $Task = [system.net.dns]::GetHostAddressesAsync($Address)
+    [Threading.Tasks.Task]::WaitAll($Task)
+    if ($Task.IsFaulted) {
+        return $true
+    }
+    
+    foreach ($Ip in $Task.Result) {
+        $TcpClient = new-Object system.Net.Sockets.TcpClient -ArgumentList $Ip.AddressFamily
+        $ConnectResult = $TcpClient.ConnectAsync($Ip, $Port).Wait($Timeout)
+        if ($ConnectResult) {
+            return $false
+        }
+    }
+    return $true
+} 
+
 Write-Host "Docker-FTW running preflight checks";
 
-if ((wsl -l).contains($distro)) {
+$dockerFtwAlreadyInstalled = (wsl -l).contains($distro)
+if ($dockerFtwAlreadyInstalled) {
     Write-Host -NoNewLine "Docker-FTW already installed. Please uninstall and run the installer again ...";
     $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown");
     Exit;
 }
 
-$dockerDaemonRunning = (tnc localhost -port $dockerPort -InformationLevel Quiet).TcpTestSucceeded;
-Clear-Host;
-
-if ($dockerDaemonRunning) 
-{
+$dockerPortAvailable = CheckPortIsAvailable -Port $dockerPort -Timeout 256
+if (!$dockerPortAvailable) {
     Write-Host -NoNewLine "Something is already running on port $dockerPort. Please uninstall it and run the installer again ...";
     $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown");
     Exit;
 }
 
 Write-Host "Docker-FTW preflight checks succeded.";
-Write-Host "Docker-FTW installing docker daemon in the WSL2.";
+
+Write-Host "Docker-FTW creating the `.docker-ftw` home folder in $HOME";
 
 $dockerFtwHome = "$HOME\\.docker-ftw\\";
 if (!(Test-Path -Path $dockerFtwHome)) {
     mkdir -p $dockerFtwHome;
 }
+
+$dockerFtwVersionFile = "$dockerFtwHome\\.version";
+Out-File -FilePath $dockerFtwVersionFile -InputObject $dockerFtwVersion -NoNewLine
+
+Write-Host "Docker-FTW installing docker daemon in the WSL2.";
 
 $dockerFtwTmp = "$dockerFtwHome\\tmp"
 mkdir -p $dockerFtwTmp;
--- README.md
+++ README.md
@@ -2,14 +2,22 @@
 
 Setup Docker for Windows withut Docker Desktop.
 
+## Installation 
+
 With PowerShell, you must ensure Get-ExecutionPolicy is not Restricted. 
 I suggest using `Bypass` to bypass the policy and get the things installed.
 
 - Open a PowerShell console (Win + x, i)
-- Run `Get-ExecutionPolicy`. If it returns `Restricted` or `RemoteSigned`, then you have to run `Set-ExecutionPolicy Bypass -Scope Process`
+- Run `Set-ExecutionPolicy Bypass -Scope Process`
 - Next run `./Install.ps1`
 - Close the PowerShell console
 - Open a new PowerShell console (Win + x, i)
 - Run `docker run -it hello-world` to confirm it works
+
+## Update
+
+Currently the update path is not implemented. To update simply uninstall and then install docker-ftw again. This will delete all docker containers, volumes etc.
+
+## Uninstallation 
 
 To uninstall everything, just run `./Uninstall.ps1`
